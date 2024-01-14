0b685b8cac35599bb0f8e5d2ea96cebd8de20cca
HoDienCong <hodiencong2000@gmail.com>
2024-01-14 23:26:37 +0700

    fix ui

--- app/page.js
+++ app/page.js
@@ -1,10 +1,26 @@
+'use client'
 import Image from "next/image";
 import Model3d from "./components/Model3d";
+import Fiber3D from "./components/Fiber3D";
+import ImageAni from "./components/ImageAni";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Model3d />
+    <main className="flex min-h-screen flex-col items-center justify-between p-4">
+      <Fiber3D />
+     
+      <div  className="h-screen w-full relative indent-1">
+        <ImageAni />
+      </div>
+      <div  className="h-screen w-full relative indent-1">
+      <ImageAni indexItem={1}/>
+      </div>
+      <div  className="h-screen w-full relative indent-1">
+      <ImageAni indexItem={2}/>
+      </div>
+      <div  className="h-screen w-full relative indent-1">
+      <ImageAni indexItem={3}/>
+      </div>
     </main>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path')
+const nextConfig = {
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+      },
+      swcMinify:true,
+      compiler:{
+        styledComponents:{
+          displayName:true,
+          ssr:true,
+          minify:true
+        }
+      },
+      images: {
+        domains: [
+          'images.unsplash.com',
+          'ipfs.pantograph.app',
+          'ipfs.pantograph.app/ipfs',
+          'ipfsgw.bountykinds.com',
+          'skywalker.infura-ipfs.io',
+          'ipfs.filebase.io',
+          'ucarecdn.com'
+        ]
+      }
+}
 
 module.exports = nextConfig
