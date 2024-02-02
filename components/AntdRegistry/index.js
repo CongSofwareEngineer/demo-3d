e38b23f35eddc2016d148c750831f6f36fa3d44c
Congbacoor <cong@bacoor.co>
2024-02-02 15:52:09 +0700

    clean code

--- components/AntdRegistry/index.js
+++ components/AntdRegistry/index.js
@@ -1,36 +1,23 @@
 'use client'
 import { useState } from 'react'
-// import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs'
 
 import { useServerInsertedHTML } from 'next/navigation'
 import { ServerStyleSheet, StyleSheetManager } from 'styled-components'
 
 const StyledComponentsRegistry = ({ children }) => {
-  // const cache = React.useMemo(() => createCache(), [])
   const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet())
 
   useServerInsertedHTML(() => {
     const styles = styledComponentsStyleSheet.getStyleElement()
     styledComponentsStyleSheet.instance.clearTag()
-    return (
-      <>{styles}</>
-    // <style id="antd" dangerouslySetInnerHTML={{ __html: extractStyle(cache, true) }} />
-    )
+    return styles
   }
   )
-  if (typeof window !== 'undefined') return <>{children}</>
+  if (typeof window !== 'undefined') return children
   return (
     <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
       {children}
     </StyleSheetManager>
-
-  // <StyleProvider
-  //   sheet={styledComponentsStyleSheet.instance}
-  //   cache={cache}
-  //   hashPriority='high'
-  // >
-  //   {children}
-  // </StyleProvider>
   )
 }
 
