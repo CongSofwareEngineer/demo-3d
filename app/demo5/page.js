95800c0a9de3bd83ca9da0c437b71f08df098d36
Congbacoor <cong@bacoor.co>
2024-03-08 16:06:33 +0700

    demo5

---
+++ app/demo5/page.js
@@ -0,0 +1,67 @@
+'use client';
+import { images, videos } from '@/config/images';
+import Image from 'next/image';
+import React from 'react';
+import styled, { css } from 'styled-components';
+import useModal from '@/hooks/useModal';
+import useSizeScreen from '@/hooks/useSizeScreen';
+import SvgOurService from './svgOurService';
+
+const VideoCustom = styled.video`
+  position: absolute !important;
+  inset: 0;
+  object-fit: cover;
+
+  ${(props) =>
+    props.isScaleWidth
+      ? css`
+          width: 100vw;
+          height: auto;
+        `
+      : css`
+          height: 100vh;
+          width: auto;
+        `}
+`;
+
+const ImageCustom = styled(Image)`
+    height: auto !important;
+    min-height: 100vh !important;
+    width: 100vw !important;
+    position: absolute !important;
+    max-width: fit-content !important;
+     width: auto !important;
+     left: 50% !important;
+    right: 50% !important;
+    transform: translate(-50%, 0%) !important;
+ `;
+
+const Demo4 = () => {
+  const { openModal } = useModal();
+  const { ratioScreen } = useSizeScreen();
+
+  console.log({ ratioScreen });
+
+  return (
+    <div className="relative w-screen h-screen overflow-hidden  ">
+      <div className="w-full h-full relative">
+        {/* <BgVideo src={images.bgVideo} fill quality={80} /> */}
+        {/* <VideoCustom
+          src={videos.planet1}
+          muted
+          autoPlay
+          playsInline
+          loop
+          controls={false}
+          preload="auto"
+          isScaleWidth={!ratioScreen.ratioBeautiful}
+        /> */}
+      </div>
+      <SvgOurService />
+      {/* <ImageCustom src={images.frameFullMain} fill /> */}
+
+    </div>
+  );
+};
+
+export default Demo4;
---
+++ app/demo5/svgOurService.js
@@ -0,0 +1,205 @@
+import { images } from '@/config/images';
+import useModal from '@/hooks/useModal';
+import Image from 'next/image';
+import React, { useEffect, useRef, useState } from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const rotate = keyframes`
+  from {
+    transform: scale(0.955) translateX(1%);
+  }
+
+  to {
+    transform: scale(1) translateX(0%); 
+  }
+`;
+const RectCustom = styled.rect`
+  /* animation: ${rotate} 2s linear infinite; */
+`;
+const ImageCustom = styled.image`
+  -o-animation: ${rotate} 2s ease-in infinite alternate;
+  -moz-animation: ${rotate} 2s ease-in infinite alternate;
+  -webkit-animation: ${rotate} 2s ease-in infinite alternate;
+  animation: ${rotate} 2s ease-in infinite alternate;
+`;
+
+const SvgOurService = () => {
+  const { openModal } = useModal()
+  const uorServiceRef = useRef(null)
+
+  useEffect(() => {
+    const ourService = document.querySelector('#image1_35_9_react')
+    console.log({ ourService });
+
+    ourService.addEventListener('mousedown', (e) => {
+      uorServiceRef.current.setAttribute('href', images.myPortfolio)
+    })
+
+    ourService.addEventListener('mouseup', (e) => {
+      uorServiceRef.current.setAttribute('href', images.ourServices1)
+    })
+  }, [])
+
+  const handleOpen = (text) => {
+    // openModal({
+    //   body: <div>{text}</div>
+    // });
+  };
+  return (
+    <div className="absolute w-full bottom-0 z-10">
+      <svg
+        width="100%"
+        height="auto"
+        viewBox="0 0 1920 1080"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+      >
+        <rect y="179" width="1920" height="100%" fill="url(#pattern0)" />
+        <g clipPath="url(#clip0_35_9)">
+          <RectCustom
+            onClick={() => handleOpen('our service')}
+            className="hover"
+            x="26"
+            y="758"
+            width="384"
+            height="346"
+            fill="url(#pattern1)"
+            id='image1_35_9_react'
+          />
+          <RectCustom
+            className="hover"
+            x="372"
+            y="726"
+            width="400"
+            height="400"
+            fill="url(#pattern2)"
+          />
+          <RectCustom
+            className="hover"
+            x="1494"
+            y="758"
+            width="400"
+            height="400"
+            fill="url(#pattern3)"
+          />
+          <RectCustom
+            className="hover"
+            x="760"
+            y="704"
+            width="400"
+            height="400"
+            fill="url(#pattern4)"
+          />
+          <RectCustom
+            className="hover"
+            x="1152"
+            y="724"
+            width="400"
+            height="400"
+            fill="url(#pattern5)"
+          />
+        </g>
+        <defs>
+          <pattern
+            id="pattern0"
+            patternContentUnits="objectBoundingBox"
+            width="1"
+            height="1"
+          >
+            <use
+              xlinkHref="#image0_35_9"
+              transform="matrix(0.000520833 0 0 0.000925926 0.00108612 -0.333627)"
+            />
+          </pattern>
+          <pattern
+            id="pattern1"
+            patternContentUnits="objectBoundingBox"
+            width="1"
+            height="1"
+          >
+            <use
+              xlinkHref="#image1_35_9"
+              transform="matrix(0.00281576 0 0 0.003125 -0.063151 0)"
+            />
+          </pattern>
+          <pattern
+            id="pattern2"
+            patternContentUnits="objectBoundingBox"
+            width="1"
+            height="1"
+          >
+            <use xlinkHref="#image2_35_9" transform="scale(0.0025)" />
+          </pattern>
+          <pattern
+            id="pattern3"
+            patternContentUnits="objectBoundingBox"
+            width="1"
+            height="1"
+          >
+            <use xlinkHref="#image3_35_9" transform="scale(0.0025)" />
+          </pattern>
+          <pattern
+            id="pattern4"
+            patternContentUnits="objectBoundingBox"
+            width="1"
+            height="1"
+          >
+            <use xlinkHref="#image4_35_9" transform="scale(0.0025)" />
+          </pattern>
+          <pattern
+            id="pattern5"
+            patternContentUnits="objectBoundingBox"
+            width="1"
+            height="1"
+          >
+            <use xlinkHref="#image5_35_9" transform="scale(0.0025)" />
+          </pattern>
+          <clipPath id="clip0_35_9">
+            <rect width="1920" height="1080" fill="white" />
+          </clipPath>
+          <ImageCustom
+            id="image1_35_9"
+            height="320"
+            ref={uorServiceRef}
+            width="400"
+            href={images.ourServices1}
+          />
+          <ImageCustom
+            id="image2_35_9"
+            width="400"
+            height="320"
+            href={images.myPortfolio}
+          />
+          <ImageCustom
+            id="image3_35_9"
+            width="400"
+            height="320"
+            href={images.aboutUs}
+          />
+          <ImageCustom
+            id="image4_35_9"
+            width="400"
+            height="320"
+            href={images.email}
+          />
+          <ImageCustom
+            id="image5_35_9"
+            width="400"
+            height="320"
+            href={images.workFlow}
+          />
+          <image
+            id="image0_35_9"
+            width="1920"
+            height="1440"
+            href={images.frameBottom}
+          />
+        </defs>
+      </svg>
+      {/* <image id="image0_33_2" width="400" height="320" href={images.ourServices1} /> */}
+    </div>
+  );
+};
+
+export default SvgOurService;
