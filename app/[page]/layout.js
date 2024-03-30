728d17e91e1954369042af1408d98d625ee499fe
HoDienCong <hodiencong2000@gmail.com>
2024-03-30 23:23:31 +0700

    fix ui

--- app/[page]/layout.js
+++ app/[page]/layout.js
@@ -1,6 +1,27 @@
+import { PAGE_EX } from '@/config/app';
+
 export async function generateMetadata ({ params, searchParams }, parent) {
+  const metaData = { description: 'coming soon' }
+  switch (params.page) {
+  case PAGE_EX.home:
+    metaData.title = 'Home'
+    break;
+  case PAGE_EX.ourService:
+    metaData.title = 'Our Service'
+    break;
+  case PAGE_EX.aboutUs:
+    metaData.title = 'About Us'
+    break;
+  case PAGE_EX.portfolio:
+    metaData.title = 'Portfolio'
+    break;
+  default:
+    metaData.title = 'Contact'
+
+    break;
+  }
   return {
-    title: 'Home'
+    ...metaData
   }
 }
 const Layout = ({ children }) => {
