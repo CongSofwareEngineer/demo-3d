8c87ff13b0328a4a10604fa4d67cbe7b97522c3a
HoDienCong <hodiencong2000@gmail.com>
2024-05-13 21:50:58 +0700

    fix ui

--- app/education/Component/Header/index.js
+++ app/education/Component/Header/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='w-full h-14 bg-slate-700 flex justify-center items-center'>
-      <div className='w-full max-w-[1350px] md:px-12 px-5 '>
+      <div className='w-full max-w-[1350px] md:px-12  pl-0' style={{ paddingLeft: 0 }}>
         <span className=' bg-white p-3 rounded-[50%] uppercase font-bold'>
            Dung BK
         </span>
